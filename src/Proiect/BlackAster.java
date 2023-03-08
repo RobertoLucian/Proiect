91cc4e8a326462549fbdacce66f9b694958f84bb
Roberto <Roberto@DESKTOP-B21FR2R>
2023-03-08 19:21:23 +0200

    Adaugare proiect

---
+++ src/Proiect/BlackAster.java
@@ -0,0 +1,111 @@
+package Proiect;
+
+public class BlackAster implements PCBuilder{
+	
+	private PC pc;
+	
+	public BlackAster()
+	{
+		this.pc=new PC();
+	}
+	
+	//Predefinire componente
+	@Override
+	public void buildProcesor() {
+		// TODO Auto-generated method stub
+		pc.setProcesor("Intel I9-13900K");
+	}
+
+	@Override
+	public void buildPlacaVideo() {
+		// TODO Auto-generated method stub
+		pc.setPlacaVideo("GeForce RTX 4090");
+	}
+
+	@Override
+	public void buildPlacaDeBaza() {
+		// TODO Auto-generated method stub
+		pc.setPlacaDeBaza("Intel Z790");
+	}
+
+	@Override
+	public void buildRam() {
+		// TODO Auto-generated method stub
+	  pc.setRam("64 GB");
+	}
+
+	@Override
+	public void buildTipRacire() {
+		// TODO Auto-generated method stub
+		pc.setTipRacire("Lichid");
+	}
+
+	@Override
+	public void buildSursa() {
+		// TODO Auto-generated method stub
+		pc.setSursa("Dedicata 1200W");
+	}
+	
+	public void buildStocare() {
+		pc.setStocare("SSD 1TB");
+		
+	}
+
+	
+	public PC getPC()
+	{
+			return this.pc;
+	}
+
+
+	@Override
+	public void buildProcesor(String Procesor) {
+		
+		
+	}
+
+
+	@Override
+	public void buildPlacaVideo(String PlacaV) {
+		
+		
+	}
+
+
+	@Override
+	public void buildPlacaDeBaza(String PlacaB) {
+	
+		
+	}
+
+
+	@Override
+	public void buildRam(String Ram) {
+		
+		
+	}
+
+
+	@Override
+	public void buildTipRacire(String TipRacire) {
+		
+		
+	}
+
+
+	@Override
+	public void buildSursa(String Sursa) {
+		
+		
+	}
+
+
+	@Override
+	public void buildStocare(String Stocare) {
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/Proiect/ComponetePC.java
@@ -0,0 +1,11 @@
+package Proiect;
+
+public interface ComponetePC {
+	public void setProcesor(String procesor);
+	public void setPlacaVideo(String placa);
+	public void setPlacaDeBaza(String placab);
+	public void setRam(String ram);
+	public void setTipRacire(String tip);
+	public void setSursa(String sursa); 
+	public void setStocare(String stocare);
+}
